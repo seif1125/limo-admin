5d3dffdb7b7051a5180be6c1041cc2441482317c
seif1125 <seifammar1125@gmail.com>
2026-05-22 14:03:01 +0300

    Fix images upload

--- src/app/dashboard/banners/add/page.js
+++ src/app/dashboard/banners/add/page.js
@@ -49,7 +49,7 @@ export default function AddBannerPage() {
         const data = new FormData();
         data.append("file", localImage.file);
         data.append("upload_preset", process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET);
-        
+        console.log(process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,'cloud name');
         const res = await fetch(`https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, { method: "POST", body: data });
         const fileData = await res.json();
         
